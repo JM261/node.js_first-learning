2b8b761b9860c650ad6a0a217138dfafbd433934
JM261 <wlaks0516@gmail.com>
2022-04-07 20:48:18 +0900

    5th commit

--- main.js
+++ main.js
@@ -2,6 +2,33 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(fileList){
+  var list = '<ul>';
+  var i = 0;
+  while(i < fileList.length){
+    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -10,60 +37,31 @@ var app = http.createServer(function(request,response){
 
     if(pathname === '/'){
       if(queryData.id === undefined){
-        var title = 'Welcome';
-        var description = 'Hello Nodejs';
-        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+
+        fs.readdir('./data', function(error, fileList){
+          var title = 'Welcome';
+          var description = 'Hello Nodejs';
+          var list = templateList(fileList);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
         });
+
+      } else {
+        fs.readdir('./data', function(error, fileList){
+          fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+            var title = queryData.id;
+            var list = templateList(fileList);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
       }
-      fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
     } else {
       response.writeHead(404);
       response.end('Not found');
     }
-
 
  
 });
