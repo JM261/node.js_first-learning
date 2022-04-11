541a5445c2113afa1a680bd4b4b24e788d8dc360
JM261 <wlaks0516@gmail.com>
2022-04-11 23:43:32 +0900

    7th commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -44,7 +44,10 @@ var app = http.createServer(function(request,response){
           var title = 'Welcome';
           var description = 'Hello Nodejs';
           var list = templateList(fileList);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+            );
           response.writeHead(200);
           response.end(template);
         });
@@ -54,7 +57,10 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
             var title = queryData.id;
             var list = templateList(fileList);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              );
             response.writeHead(200);
             response.end(template);
           });
@@ -65,16 +71,16 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var list = templateList(fileList);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
-              <p><input text="text" placeholder="title"></p>
+          <form action="/create_process" method="post">
+              <p><input text="text" name="title" placeholder="title"></p>
               <p>
-                  <textarea placeholder="description"></textarea>
+                  <textarea name="description" placeholder="description"></textarea>
               </p>
               <p>
                   <input type="submit">
               </p>
           </form>
-        `);
+        `, '');
         response.writeHead(200);
         response.end(template);
       });
@@ -85,11 +91,55 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var tilte = post.title;
+          var title = post.title;
           var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf-8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end('success');
+          });
       });
-      response.writeHead(200);
-      response.end('success');
+    } else if(pathname === '/update'){
+      fs.readdir('./data', function(error, fileList){
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(fileList);
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="#{title}">
+                <p><input text="text" name="title" placeholder="title" value="${title}"></p>
+                <p>
+                    <textarea name="description" placeholder="description">${description}</textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            );
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    } else if(pathname === '/update_process'){
+      var body ='';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf-8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+          })
+          console.log(post);
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
